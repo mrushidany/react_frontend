50f014384aa691a2efab792901738554d0088681
mrushidany <mrushidany@gmail.com>
2021-12-01 13:46:47 +0300

    Dashboard Component Rendering

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,6 +1,14 @@
 import React, { useContext } from "react";
 import { AppContext } from "../contexts/AppContext";
+
 const Dashboard = () => {
   const appContext = useContext(AppContext);
+  const { userName, logout } = appContext;
+
+  return (
+    <div>
+      <p>Welcome to the Dashboard</p>
+    </div>
+  );
 };
 export default Dashboard;
